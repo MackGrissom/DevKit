d225f721981998c80c4e280769daac6a8ab41a0e
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-11-12 11:48:53 +0100

    17: list options

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -9,6 +9,8 @@ import { useAction } from "@/hooks/use-action";
 import { updateList } from "@/actions/update-list";
 import { FormInput } from "@/components/form/form-input";
 
+import { ListOptions } from "./list-options";
+
 interface ListHeaderProps {
   data: List;
 };
@@ -101,6 +103,10 @@ export const ListHeader = ({
           {title}
         </div>
       )}
+      <ListOptions
+        onAddCard={() => {}}
+        data={data}
+      />
     </div>
   );
 };
